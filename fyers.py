7b1a731edaf39941d58554b3a6d2a160b65ce10e
uday <udaych20@gmail.com>
2023-09-30 12:43:06 +0530

    fyers-app-2.0

--- fyers.py
+++ fyers.py
@@ -13,6 +13,9 @@ from datetime import datetime, timedelta
 # import os 
 import db
 import login
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import the CORS class
+import logging
 
 # Reading configuration from database - fyers_config table
 redirect_uri= db.getConfgValueByKey("redirect_uri")  
@@ -28,118 +31,6 @@ appSession = fyersModel.SessionModel(client_id = client_id, redirect_uri = redir
 access_token = login.authenticate(appSession.generate_authcode(),appSession)
 
 fyers = fyersModel.FyersModel(token=access_token,is_async=False,client_id=client_id,log_path="")
-
-
-
-
-# # ###############################################################################################
-# new_token = f"{client_id}:{access_token}"
-
-# auth_code=db.getConfgValueByKey("auth_code")
-
-# data = {"symbol":"NSE:NIFTYBANK-INDEX","resolution":"1","date_format":"0","range_from":"1695008700","range_to":"1695008820","cont_flag":"0"}
-# hist = fyers.history(data)
-# print(hist)
-# sdata = pandas.DataFrame(hist["candles"])
-# sdata.columns = ['date','open','high','low','close','volume']
-# sdata['date'] = pandas.to_datetime(sdata['date'], unit='s', origin='unix', utc=True).dt.tz_convert('Asia/Kolkata')
-# sdata['date'] = sdata['date'].dt.strftime('%Y-%m-%d %H:%M')
-# print(sdata)
-# # ###############################################################################################
-
-
-
-# ###############################################################################################
-
-# def onmessage(message):
-#     """
-#     Callback function to handle incoming messages from the FyersDataSocket WebSocket.
-
-#     Parameters:
-#         message (dict): The received message from the WebSocket.
-
-#     """
-#     query = "insert into stock_data(symbol,exch_feed_time,ltp,day_high,day_low,day_open,prev_close_price) values (%s,%s,%s,%s,%s,%s,%s)"
-#     query1 = "insert into stock_resp(message) values (%s)"
-#     if message["ltp"]:
-#         db.update_symbol_data(query1,message)
-#         # print(message)
-#         # json = message
-#         # symbol = message["symbol"]
-#         # exch_feed_time = db.convert_epoch_to_date(message["exch_feed_time"])
-#         # ltp = message["ltp"],
-#         # day_high = message["high_price"]
-#         # day_low = message["low_price"]
-#         # day_open = message["open_price"]
-#         # prev_close_price = message["prev_close_price"]
-#         # print(query,symbol,exch_feed_time,ltp,day_high,day_low,day_open,prev_close_price)
-        
-      
-
-
-# def onerror(message):
-#     """
-#     Callback function to handle WebSocket errors.
-
-#     Parameters:
-#         message (dict): The error message received from the WebSocket.
-
-
-#     """
-#     print("Error:", message)
-
-
-# def onclose(message):
-#     """
-#     Callback function to handle WebSocket connection close events.
-#     """
-#     print("Connection closed:", message)
-
-
-# def onopen():
-#     """
-#     Callback function to subscribe to data type and symbols upon WebSocket connection.
-
-#     """
-#     # Specify the data type and symbols you want to subscribe to
-#     data_type = "SymbolUpdate"
-
-#     # Subscribe to the specified symbols and data type
-#     # symbols = ["NSE:NIFTY23SEPFUT","NSE:NIFTY50-INDEX" , "NSE:NIFTYBANK-INDEX"]
-#     symbols = ["NSE:NIFTYBANK-INDEX"]
-#     # ["MCX:NATURALGAS23SEPFUT"]
-#     fyersSocket.subscribe(symbols=symbols, data_type=data_type)
-
-#     # Keep the socket running to receive real-time data
-#     fyersSocket.keep_running()
-
-
-
-# # Create a FyersDataSocket instance with the provided parameters
-# fyersSocket  = data_ws.FyersDataSocket(
-#     access_token=new_token,          # Access token in the format "appid:accesstoken"
-#     log_path="",                     # Path to save logs. Leave empty to auto-create logs in the current directory.
-#     litemode=False,                   # Lite mode disabled. Set to True if you want a lite response.
-#     write_to_file=True,             # Save response in a log file instead of printing it.
-#     reconnect=True,                  # Enable auto-reconnection to WebSocket on disconnection.
-#     on_connect=onopen,               # Callback function to subscribe to data upon connection.
-#     on_close=onclose,                # Callback function to handle WebSocket connection close events.
-#     on_error=onerror,                # Callback function to handle WebSocket errors.
-#     on_message=onmessage             # Callback function to handle incoming messages from the WebSocket.
-# )
-
-# fyersSocket.connect()
-
-
-# ######################################################################################################################
-
-
-# symbols=db.getConfgValueByKey("auth_code")
-
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import the CORS class
-import logging
-
 
 
 app = Flask(__name__)
@@ -161,12 +52,6 @@ def get_index_option_expiry():
     flat_array = [date for sublist in data for date in sublist]
     return jsonify(flat_array)
 
-# start = 43700
-# end = 45400
-# frequency = 100
-# values_list = []
-# for value in range(start, end + 1, frequency):
-#    values_list.append(value)
 
 @app.route('/api/index-strike', methods=['POST'])
 def get_index_strike():
@@ -227,7 +112,7 @@ def call_buy():
         
         # NSE:BANKNIFTY23SEP44600CE
         option_string = 'NSE:'+index+formatted_date+strike+optionType
-        
+        print('buy - ',option_string)
         data =  {
         "symbol":option_string,
         "qty":quantity,
